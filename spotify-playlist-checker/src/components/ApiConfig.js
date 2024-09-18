7ac37fc1eb7dd8583410dd377e2bf61232f6e28c
Phil Chandy <phillipchandy200@gmail.com>
2024-09-18 14:50:58 -0400

    update build

--- spotify-playlist-checker/src/components/ApiConfig.js
+++ spotify-playlist-checker/src/components/ApiConfig.js
@@ -1,3 +1,3 @@
-const apiBaseUrl = "ec2-18-222-149-241.us-east-2.compute.amazonaws.com"
+const apiBaseUrl = "http://ec2-18-222-149-241.us-east-2.compute.amazonaws.com:3001"
 
 export default apiBaseUrl;
